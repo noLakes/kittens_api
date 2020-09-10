02e5b1b6d5967e55fd9a5ee32c8ee8d14d95ff32
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-09 20:42:07 -0400

    setup kittens controller and routes

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,31 @@
+class KittensController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
+end
